d5310d74381bfb25278c1b5c48ed7f562f885b11
Alexandr Kolomytski <alexandr.kolom.web@yandex.ru>
2022-10-05 17:22:22 +1000

    plugins for test

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -24,7 +24,11 @@ export default function buildPlugins({
 
   if (isDev) {
     plugins.push(new webpack.HotModuleReplacementPlugin());
-    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+    plugins.push(new BundleAnalyzerPlugin(
+      {
+        openAnalyzer: false,
+      },
+    ));
   }
 
   return plugins;
